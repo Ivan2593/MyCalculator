ea1135c129feda807cab26cfa7cd11a4cce1301e
Ivan2593 <i.drobyshevskii@g.nsu.ru>
2022-06-12 17:51:44 +0700

    Add project files.

---
+++ MyCalculator/Helpers/ServicesContainer.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MyCalculator.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalculator.Helpers
+{
+    public sealed class ServicesContainer
+    {
+
+        public MainPageViewModel mainPageViewModel => GetService<MainPageViewModel>();
+
+        /// <summary>
+        /// Возвращает запрашиваемый сервис из контейнера.
+        /// </summary>
+        /// <typeparam name="T"> Имя сервиса </typeparam>
+        /// <returns>Сервис</returns>
+        public static T GetService<T>() => App.ServicesContainer.GetRequiredService<T>();
+    }
+}
---
+++ MyCalculator/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalculator.ViewModel
+{
+    /// <summary>
+    /// Класс управления главной страницей
+    /// </summary>
+    public sealed class MainPageViewModel: ObservableObject
+    {
+        /// <summary>
+        /// Строка для вывода
+        /// </summary>
+        private string _mainString = "0";
+
+        /// <summary>
+        /// Строка для вывода
+        /// </summary>
+        public string MainString
+        {
+            get => _mainString;
+            private set
+            {
+                if(_mainString != value)
+                {
+                    _mainString = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Команда говна
+        /// </summary>
+        public RelayCommand SomeAddCommand { get; }
+
+        /// <summary>
+        /// Инициализирует класс <see cref="MainPageViewModel"/>
+        /// </summary>
+        public MainPageViewModel()
+        {
+            SomeAddCommand = new RelayCommand(SomeAdd);
+        }
+
+        /// <summary>
+        /// Изменение строки
+        /// </summary>
+        private void SomeAdd()
+        {
+            MainString = MainString + "a";
+        }
+    }
+}
